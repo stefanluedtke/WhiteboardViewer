fb39732bee48f61d6ba5d21bd56ff7b9aa861459
Stefan Luedtke <stefan_luedtke@gmx.net>
2015-06-26 14:44:13 +0200

    weiter an Event-Verarbeitungs-Methoden

--- src/de/mmis/applications/whiteboardviewer/WhiteboardViewerImpl.java
+++ src/de/mmis/applications/whiteboardviewer/WhiteboardViewerImpl.java
@@ -1,6 +1,7 @@
 package de.mmis.applications.whiteboardviewer;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -23,10 +24,23 @@ public class WhiteboardViewerImpl extends AbstractObservable<Event> implements W
 	 * Positions of each tag, 0 if not positioned
 	 * 1-9 for tables 1-9
 	 */
-	Map<String , String> sittingPositions;
-	String lecturerPosition;
+	Map<String , Integer> sittingPositions;
+	/**
+	 * number of whiteboard where lecuturer currently stands,
+	 * not necessarily the same as variable whiteboard,
+	 * if lecturer has just moved to this position
+	 */
+	int lecturerPosition;
 	int nrPensTaken;
 	AXISHTTPv2 camera;
+	/**
+	 * Whiteboard that is currently shown
+	 */
+	int whiteboard;
+	/**
+	 * Number of screen where output is currently shown
+	 */
+	int displayPosition;
 	static final int[][] viewQualityMatrix = 
 		{{-1,0,1,1,1,1,-1,-1,-1},
 		{-1,-1,-1,-1,1,1,1,1,0},
@@ -35,14 +49,47 @@ public class WhiteboardViewerImpl extends AbstractObservable<Event> implements W
 		{0,-1,-1,-1,-1,-1,1,1,1},
 		{1,-1,-1,-1,-1,-1,0,1,1},
 		{1,1,1,0,-1,-1,-1,-1,-1}};
-			
+		
+	static List<List<Integer>> badViewTable;
 	
 	public WhiteboardViewerImpl(){
-		sittingPositions = new HashMap<String,String>();
+		sittingPositions = new HashMap<String,Integer>();
 		//new tags are added with addListenerID
 		
-		lecturerPosition="0";
+		//build badViewTable
+		badViewTable=new ArrayList<List<Integer>>();
+		List<Integer> bad0 = new ArrayList<Integer>();
+		bad0.add(1);
+		bad0.add(7);
+		bad0.add(8);
+		bad0.add(9);
+		badViewTable.add(0, bad0);
+		List<Integer> bad1 = new ArrayList<Integer>();
+		bad1.add(1);
+		bad1.add(4);
+		bad1.add(3);
+		bad1.add(2);
+		badViewTable.add(1, bad1);
+		List<Integer> bad2 = new ArrayList<Integer>();
+		bad2.add(5);
+		bad2.add(6);
+		bad2.add(4);
+		bad2.add(3);
+		bad2.add(2);
+		badViewTable.add(2, bad2);
+		List<Integer> bad3 = new ArrayList<Integer>();
+		bad3.add(6);
+		bad3.add(5);
+		bad3.add(9);
+		bad3.add(8);
+		bad3.add(7);
+		badViewTable.add(3, bad3);
+		
+		
+		lecturerPosition=0;
 		nrPensTaken=0;
+		whiteboard=0;
+		displayPosition=0;
 		
 		//Connect to camera
 		try {
@@ -66,40 +113,39 @@ public class WhiteboardViewerImpl extends AbstractObservable<Event> implements W
 			nrPensTaken++;
 			
 			//get whiteboard where lecturer writes on
-			int whiteboard=Integer.parseInt(lecturerPosition);
+			whiteboard=lecturerPosition;
 			//get screen where to show whiteboard
-			int displayPosition = getDisplayPosition(whiteboard);
+			displayPosition = getDisplayPosition(whiteboard);
 			
-			//adjust camera
-			//TODO welcher preset zu welcher nummer??
-			try {
-				camera.moveToPreset(whiteboard);
-			} catch (IOException e) {
-				System.err.println("Error adjusting camera");
-				e.printStackTrace();
-			}
 			
 			//permanently get camera image and show on displayPosition
 			//->Thread
+			startOutput();
 			//TODO
 			
 		}else if(event.getEventType()==Type.PENINSERTED){
-			//weiter anzeigen wie bisher, aber keine 
-			//aenderungen der whiteboard- oder anzeigeposition
-			//mehr machen
+			//if more than 1 pen or 0 pens are taken: do nothing
+			if(nrPensTaken>=2){
+				nrPensTaken--;
+				return;
+			}else if(nrPensTaken==0){
+				return;
+			}else{//nrPensTaken==1
+				nrPensTaken=0;
+				stopOutput();
+			}
+			
 		}	
 	}	
 	
+
 	@Override
 	/**
 	 * The id is the name of the hmm (@ID in GP)
 	 */
 	public void processListenerHMMEvent(Map<State<List<Double>>,Double> stateProbability, String id){
-		//sitzpositionen aktualisieren (jedes mal?)
-		
-		//(wenn eine wahrscheinlichkeit >0.5)
-		
-		//iteriere over all states, find state with max prob
+		//update sitting positions (every time?)
+		//iterate over all states, find state with max prob
 		double maxprob = 0;
 		State<List<Double>> maxprobstate=null;
 		for(State<List<Double>> state:stateProbability.keySet()){
@@ -110,26 +156,40 @@ public class WhiteboardViewerImpl extends AbstractObservable<Event> implements W
 			}
 		}
 		
-		//add (tag_id, state (^=table) to sittingPositions
-		sittingPositions.put(id,maxprobstate.getName());
+		//add (tag_id, state (^=table)) to sittingPositions
+		sittingPositions.put(id,Integer.parseInt(maxprobstate.getName()));
 		
-		//TODO noch was zu tun?
+		//TODO what else?
 		
 	}
 	
 	@Override
 	public void processScreenHMMEvent(Map<State<List<Double>>,Double> stateProbability){
 		
-		//if(prob(aktuellerscreen)<0.5):
-			//anzeigeposition aktualisieren
+		double maxprob = 0;
+		State<List<Double>> maxprobstate=null;
+		for(State<List<Double>> state:stateProbability.keySet()){
+			double prob = stateProbability.get(state);
+			if(prob>maxprob){
+				maxprob=prob;
+				maxprobstate=state;
+			}
+		}
+		
+		lecturerPosition=Integer.parseInt(maxprobstate.getName());
+		
+		//TODO do not update immediately, 
+		//update output position
+		whiteboard=lecturerPosition;
+		displayPosition=getDisplayPosition(whiteboard);
 	}
 	
 
 	/**
 	 * get display position using whiteboardPosition
-	 * and sittingPositions
+	 * and sittingPositions (variable displayPosition is not changed)
 	 */
-	public int getDisplayPosition(int whiteboard){
+	protected int getDisplayPosition(int whiteboard){
 		//TODO 
 		return 0;
 	}
@@ -140,8 +200,30 @@ public class WhiteboardViewerImpl extends AbstractObservable<Event> implements W
 	 * @param tagID
 	 */
 	public void addListenerID(String id){
-		sittingPositions.put(id, "0");
+		sittingPositions.put(id, 0);
 	}
+	
+	protected void startOutput() {
+		// TODO	as a thread
+		//adjust camera
+		
+		//TODO which preset for which number?
+		//TODO only if whiteboard changed(?)
+		try {
+			camera.moveToPreset(whiteboard);
+		} catch (IOException e) {
+			System.err.println("Error adjusting camera");
+			e.printStackTrace();
+		}
+		
+		
+	}
+	
+	protected void stopOutput(){
+		//TODO
+	}
+	
+	
 
 
 }
